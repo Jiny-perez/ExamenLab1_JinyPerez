759a58286f9bcf70912450c3fa28f6af52f25422
Jiny-perez <jinyperez55@gmail.com>
2025-05-23 13:06:36 -0600

    Examen de Jiny Perez

---
+++ src/perez_jiny_exam1/Perez_Jiny_Exam1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perez_jiny_exam1;
+
+
+public class Perez_Jiny_Exam1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
